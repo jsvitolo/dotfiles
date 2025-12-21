46f2d84fd780221517d30faa6af2d89f7270ef76
Jones Vitolo <jsvitolo@gmail.com>
2025-12-21 06:49:48 -0300

    chore: wip initial neovim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,27 +1,15 @@
--- Neovim config
--- Managed with GNU Stow
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
--- Leader key
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
--- Opcoes basicas
-vim.opt.number = true           -- Numeros de linha
-vim.opt.relativenumber = true   -- Numeros relativos
-vim.opt.mouse = "a"             -- Mouse habilitado
-vim.opt.ignorecase = true       -- Busca case-insensitive
-vim.opt.smartcase = true        -- Busca case-sensitive se tiver maiuscula
-vim.opt.hlsearch = false        -- Nao destacar busca
-vim.opt.wrap = false            -- Nao quebrar linhas
-vim.opt.breakindent = true      -- Indentacao em linhas quebradas
-vim.opt.tabstop = 2             -- Tab = 2 espacos
-vim.opt.shiftwidth = 2          -- Indentacao = 2 espacos
-vim.opt.expandtab = true        -- Tab vira espacos
-vim.opt.termguicolors = true    -- Cores 24-bit
-vim.opt.signcolumn = "yes"      -- Coluna de sinais sempre visivel
-vim.opt.updatetime = 250        -- Tempo de atualizacao
-vim.opt.clipboard = "unnamedplus" -- Clipboard do sistema
-
--- Atalhos basicos
-vim.keymap.set("n", "<leader>w", "<cmd>write<cr>", { desc = "Salvar" })
-vim.keymap.set("n", "<leader>q", "<cmd>quit<cr>", { desc = "Sair" })
+require("vim-options")
+require("lazy").setup("plugins")
