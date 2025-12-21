d7338cc9359fd431f6df6f1286d4239e0fef50a9
Jones Vitolo <jsvitolo@gmail.com>
2025-12-21 08:19:25 -0300

    chore: improve theme

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -10,21 +10,32 @@ return {
           integrations = {
             telescope = {
               enabled = true,
-              style = "nvchad",
             },
           },
         })
         vim.cmd.colorscheme "catppuccin-mocha"
 
         -- Telescope transparent background
-        vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { bg = "NONE" })
+        local function set_telescope_hl()
+          local hl = vim.api.nvim_set_hl
+          hl(0, "TelescopeNormal", { bg = "NONE" })
+          hl(0, "TelescopeBorder", { bg = "NONE", fg = "#89b4fa" })
+          hl(0, "TelescopePromptNormal", { bg = "NONE" })
+          hl(0, "TelescopePromptBorder", { bg = "NONE", fg = "#89b4fa" })
+          hl(0, "TelescopePromptTitle", { bg = "NONE", fg = "#94e2d5" })
+          hl(0, "TelescopeResultsNormal", { bg = "NONE" })
+          hl(0, "TelescopeResultsBorder", { bg = "NONE", fg = "#89b4fa" })
+          hl(0, "TelescopeResultsTitle", { bg = "NONE", fg = "#94e2d5" })
+          hl(0, "TelescopePreviewNormal", { bg = "NONE" })
+          hl(0, "TelescopePreviewBorder", { bg = "NONE", fg = "#89b4fa" })
+          hl(0, "TelescopePreviewTitle", { bg = "NONE", fg = "#94e2d5" })
+        end
+
+        set_telescope_hl()
+
+        vim.api.nvim_create_autocmd("ColorScheme", {
+          callback = set_telescope_hl,
+        })
       end
     }
   }
